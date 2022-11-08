2ca6a4d5d317027925f855b22bc9ff70bf2f875f
ahmed elgendy <ahmed.elgendy@innovitics.com>
2022-11-08 12:49:10 +0200

    editing app theme and adding scroll to convert fragment

--- app/src/main/java/com/ahmedelgendy/banquemisrtask/convert/presentation/ConvertFragment.kt
+++ app/src/main/java/com/ahmedelgendy/banquemisrtask/convert/presentation/ConvertFragment.kt
@@ -7,10 +7,12 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
+import androidx.lifecycle.lifecycleScope
 import com.ahmedelgendy.banquemisrtask.databinding.ConvertFragmentLayoutBinding
 import com.ahmedelgendy.banquemisrtask.general.network.Resource
 import com.ahmedelgendy.banquemisrtask.general.showLongToast
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.launch
 
 @AndroidEntryPoint
 class ConvertFragment : Fragment() {
@@ -34,39 +36,47 @@ class ConvertFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-
-        viewModel.getCurrencies()
-
-
-        viewModel.currenciesResponse.observe(viewLifecycleOwner) {
-            it.getContentIfNotHandled()?.let { event ->
-                when (event) {
-                    is Resource.Success -> {
-                        val status = event.value.success
-                        val response = event.value.symbols
-
-                        if (status == true) {
-
-
-                        }
-                    }
-                    is Resource.Failure -> {
-                        showLongToast("${event.errorCode} ${event.cause}", requireContext())
-                    }
-                    Resource.Loading -> {
-                        Log.d("TAG", "request loading ")
-                    }
-                }
-
-            }
-
-        }
+        loadData()
 
     }
 
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
+    }
+
+
+    fun loadData() {
+        viewModel.getCurrencies()
+        startObserver()
+    }
+
+    fun startObserver() {
+        lifecycleScope.launch() {
+            viewModel.currenciesResponse.observe(viewLifecycleOwner) {
+                it.getContentIfNotHandled()?.let { event ->
+                    when (event) {
+                        is Resource.Success -> {
+                            val status = event.value.success
+                            val response = event.value.symbols
+
+                            if (status == true) {
+
+
+                            }
+                        }
+                        is Resource.Failure -> {
+                            showLongToast("${event.errorCode} ${event.cause}", requireContext())
+                        }
+                        Resource.Loading -> {
+                            Log.d("TAG", "request loading ")
+                        }
+                    }
+
+                }
+
+            }
+        }
     }
 
 
